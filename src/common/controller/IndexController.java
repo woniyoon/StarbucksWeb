27875949afec4506007526d08139e10e65ff3b87
woniyoon <gellifisch@gmail.com>
2020-06-19 14:36:05 +0900

    Create IndexController

---
+++ src/common/controller/IndexController.java
@@ -0,0 +1,14 @@
+package common.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class IndexController extends AbstractController {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		super.setRedirect(true);
+		super.setViewPage("/WEB-INF/index.sb");
+	}
+
+}
