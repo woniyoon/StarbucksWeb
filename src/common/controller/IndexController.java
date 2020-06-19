b7f3e6515fab095ec7e83acaae7b1ba8aebc3ceb
woniyoon <gellifisch@gmail.com>
2020-06-19 14:36:42 +0900

    Fix url mapping issue

--- src/common/controller/IndexController.java
+++ src/common/controller/IndexController.java
@@ -8,7 +8,7 @@ public class IndexController extends AbstractController {
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		super.setRedirect(true);
-		super.setViewPage("/WEB-INF/index.sb");
+		super.setViewPage("/WEB-INF/index.jsp");
 	}
 
 }
