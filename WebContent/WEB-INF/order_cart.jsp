f1ec77639c3d749a52b0adad44b8bd459c15ef99
woniyoon <gellifisch@gmail.com>
2020-06-20 13:36:27 +0900

    Create js, css, jsp files for order_cart

---
+++ WebContent/WEB-INF/order_cart.jsp
@@ -0,0 +1,309 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="/StarbucksWeb/css/order_cart.css"></style>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
+    <script type="text/javascript" src="/StarbucksWeb/js/order_cart.js"></script>
+    <title>스타벅스 코리아</title>
+<style type="text/css">
+
+	body {
+	    position: absolute;
+	    min-height: 100vh;
+	    height: auto;
+	    width: 100vw;
+	    margin: 0 auto;
+	    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+	    /* border: 1px solid pink; */
+	}
+	
+	header {
+	    position: sticky;
+	    display: flex;
+	    align-items: center;
+	    top: 0;
+	    left: 0;
+	    height: 10%;
+	    background-color: white;
+	    box-shadow: 1px 1px 1px 0 #b6b6b65a, 0 1px 1px 0 rgba(155, 155, 155, 0.337);
+	    padding-left: 30px;
+	}
+	
+	ul.nav_menu {
+	    list-style-type: none;
+	    /* border: 2px solid peru; */
+	    display: inline-block;
+	}
+	
+	li {
+	    display: inline-block;
+	    font-weight: 500;
+	    font-size: 15pt;
+	    margin-right: 15px;
+	}
+	
+	.nav_menu a:active {
+	    color: #006633;
+	}
+	
+	img#logo {
+	    vertical-align: middle;
+	    display: inline-block;
+	}                               /* header 끝 */
+	
+	nav {
+	    display: flex;
+	    justify-content: flex-end;
+	    width: 50%;
+	    margin: 30px auto;
+	    /* border: 1px solid salmon; */
+	}
+	
+	ul.order_progress li {
+	    /* display: inline-block; */
+	    display: table-cell;
+	    vertical-align: middle;
+	    height: 50px;
+	    min-width: 80px;
+	    background-color: white;
+	    color: black;
+	    border: 1px solid lightgrey;
+	    margin: 0;
+	    text-align: center;
+	}
+	
+	ul.order_progress {
+	    display: table;
+	    margin: 0 auto;
+	    /* justify-content: center; */
+	    padding-inline-start: 0px;
+	}
+	
+	section {
+	    margin: 30px auto;
+	    width: 100%;
+	    height: auto;
+	    padding-bottom: 5rem;
+	}
+	
+	div.card {
+	    /* display: flex;
+	    align-items: row; */
+	    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+	    max-width: 500px;
+	    max-height: 600px;
+	    margin: 50px auto;
+	    padding: 30px;
+	    border-radius: 2%;
+	}      
+	
+	@media (max-width:650px) {
+	    div.card {
+	        min-width: 60vw;
+	        max-width: 60vw;
+	        max-height: 600px;
+	        min-height: 40vh;
+	        /* border: 2px solid black; */
+	        overflow: inherit;
+	        font-size: 0.8em;
+	    }
+	
+	    div.card label {
+	        font-size: 0.8em;
+	        height: 80%;
+	    }
+	    
+	    div.card_detail_container {
+	        /* border: 2px solid black; */
+	        display: flex;
+	        flex-direction: column;
+	        align-items: center;
+	    }
+	
+	    div.card_detail_container > img {
+	        flex: 1;
+	        max-width: 150px;
+	        max-height: 180px;
+	        /* border: 2px solid black; */
+	    }
+	
+	    div#card_text {
+	        display: flex;
+	        flex: 3;
+	        flex-direction: column;
+	        /* border: 1px solid pink; */
+	    }
+	}
+	
+	.remove_button {
+	    font-size: 2rem;
+	    background-color: transparent;  
+	    border: none;
+	}
+	
+	div.card_detail_container {
+	    display: flex;
+	}
+	
+	h3#menu_name {
+		margin: 10px 0;
+	}
+	
+	div.card_detail_container > img {
+	    flex: 1;
+	    width: 200px;
+	    height: 220px;
+	}
+	
+	div#card_text {
+	    display: flex;
+	    flex: 3;
+	    flex-direction: column;
+	    align-items: center;
+	    /* border: 1px solid pink; */
+	}
+	
+	div.card ul {
+	    /* border: 1px solid purple; */
+	    text-align: start;
+	    padding: 0px
+	}
+	
+	div.card li {
+	    display: block;
+	}
+	
+	div.card label {
+	    display: inline-block;
+	    width: 70px;
+	    font-size: 1rem;
+	    /* border: 1px solid red; */
+	}
+	
+	input, select{
+	    width: 60px;
+	    box-sizing:border-box;
+	}
+	
+	span.price {
+	    display: block;
+	    font-size: 1.3rem;
+	    font-weight: bold;
+	    /* border: 1px solid red; */
+	}
+	
+	p {
+	    display: block;
+	    text-align: end;
+	    top: 0px;
+	    right: 0px;
+	    margin: 0;
+	}
+	
+	.move_button {
+	    width: 100px;
+	    height: 50px;
+	    margin: 0 5px 0 5px;
+	    background-color: #f4f4f2;
+	    border: none;
+	    border-radius: 3px;
+	    box-shadow: 1px 1px 1px 0 #b6b6b65a, 0 1px 1px 0 rgba(155, 155, 155, 0.337);
+	}
+	
+	button#next {
+	    background-color: #006633;
+	    color: #fff;
+	}
+	
+	/* section 끝 */
+	
+	#snackbar {
+	    visibility: hidden;
+	    min-width: 250px;
+	    margin-left: -125px;
+	    background-color: #006633;
+	    color: #fff;
+	    text-align: center;
+	    border-radius: 2px;
+	    padding: 16px;
+	    position: fixed;
+	    z-index: 1;
+	    left: 50%;
+	    bottom: 50px;
+	    font-size: 17px;
+	}
+	
+	hr {
+	    max-width: 50%; 
+	}
+	
+	h2#total_price {
+	    margin-right: 25vw;
+	}
+	
+	#snackbar.show {
+	    visibility: visible;
+	    -webkit-animation: fadein 0.5s, fadeout 0.5s 2.5s;
+	    animation: fadein 0.5s, fadeout 0.5s 2.5s;
+	}
+	
+	@-webkit-keyframes fadein {
+	    from {bottom: 0; opacity: 0;} 
+	    to {bottom: 50px; opacity: 1;}
+	}
+	
+	@keyframes fadein {
+	    from {bottom: 0; opacity: 0;}
+	    to {bottom: 50px; opacity: 1;}
+	}
+	
+	@-webkit-keyframes fadeout {
+	    from {bottom: 50px; opacity: 1;} 
+	    to {bottom: 0; opacity: 0;}
+	}
+	
+	footer {
+	    position: absolute;
+	    height: 5rem;
+	    bottom: 0;
+	    width: 100vw;
+	    background-color: rgb(46, 46, 46);
+	}
+
+</style>
+</head>
+<body>
+    <header>
+        <img id="logo" src="/StarbucksWeb/images/common/starbucks_logo.png" width=80px height=80px />
+        <ul class="nav_menu">
+            <li><a>ABOUT</a></li>
+            <li><a>MENU</a></li>
+            <li><a>MYPAGE</a></li>
+            <li><a>Q&A</a></li>
+        </ul>
+    </header>
+    <nav><a>HOME</a> > <a>메뉴</a> > <a id="current_nav_menu"></a></nav>
+    <section>
+        <ul class="order_progress">
+            <li id="shoppingCart"><span>커스텀</span></li>
+            <li id="order_location"><span>매장선택</span></li>
+            <li id="order_payment"><span>결제하기</span></li>
+            <li id="order_confirmed"><span>결제완료</span></li>
+        </ul>
+        <div class="items_container">
+            
+        </div>        
+        <div>
+            <button class="move_button">뒤로</button>
+            <button class="move_button" id="next">다음</button>
+        </div>
+    </section>
+    <div id="snackbar">알림용 스낵바입니다</div>
+    <footer></footer>
+</body>
+</html>
---
+++ WebContent/js/order_cart.js
@@ -0,0 +1,204 @@
+var cart = [
+    {
+        name: "아이스 아메리카노",
+        price: 4100,
+        size: 1,
+        amount: 1,
+        shots: 3,
+    },
+    {
+        name: "아이스 아메리카노",
+        price: 4100,
+        size: 1,
+        amount: 1,
+        shots: 3,
+    },
+    {
+        name: "아이스 라떼",
+        price: 4500,
+        size: 1,
+        amount: 1,
+        shots: 3,
+    },
+    {
+        name: "돌체라떼",
+        price: 4500,
+        size: 1,
+        amount: 1,
+        shots: 3,
+    },
+];
+
+var size_dictionary = { 1: "톨", 2: "그란데", 3: "벤티" };
+var num_to_size = { 1: "tall", 2: "grande", 3: "venti" };
+var shot_dictionary = { tall: 3, grande: 4, venti: 5 };
+
+$(document).ready(function(){
+    var path = window.location.pathname;
+    var start_point = path.lastIndexOf("/")+1;
+    var current_state = path.substring(start_point, path.length-3);
+    console.log(current_state);
+    
+    $("#"+current_state).css({"color": "white", "background-color": "#006633"})
+
+    $("#current_nav_menu").text($("li#"+current_state+" > span").text());
+
+    show_items();
+
+    $("select.size").change(function(){
+        var selected_size = this.value;
+        var index= (this.id).substring(4);
+        console.log("사이즈 변경 시점");
+        console.log("장바구니 속 사이즈 " + cart[index].size);
+        console.log("실제 선택된 사이즈 " + selected_size);
+        
+        console.log(cart[index].price);
+        $("#shot"+index).val(shot_dictionary[num_to_size[selected_size]]);
+
+        $("input#shot"+index).trigger("change");
+    });
+
+    $("button#next").click(function(){
+        console.log("test");
+        location.href='../order_location.html';
+
+    });
+
+});
+
+function show_items(){
+    var cart_items = $("div.items_container");
+    var items_html = "";
+    var sum = 0;
+    var sum_html = "";
+
+    if(cart.length > 0) {
+        cart.forEach(function(item, index){
+            sum += item.price;
+
+            items_html += ("<div id='card"+index+"' class='card'>"
+                                +"<p><button class='remove_button' id='"+index+"' onclick='remove_item(this.id)'>×</button></p>"
+                                +"<div class='card_detail_container'>"
+                                    + "<img width='120px' height='120px' src='/StarbucksWeb/images/bobae/1-2.jpg'>"
+                                    + "<div id='card_text' >"
+                                        + "<h3 id='menu_name'>"+item.name+"</h3>"
+                                        + "<form id='order_form'>"
+                                            +"<ul>"
+                                                + "<li><label>사이즈</label>"
+                                                    +"<select class='size' id='size"+index+"' name='size'>"
+                                                        + "<option value='1' checked>톨</option>"
+                                                        + "<option value='2'>그란데</option>"
+                                                        + "<option value='3'>벤티</option>"
+                                                    + "</select></li>"
+                                                + "<li><label>커피</label><input class='shot' id='shot"+index+"' onchange='update_shot(this)' type='number' value='"+cart[index].shots+"' min='1' max='5' ></li>"
+                                                + "<li><label>우유</label>"
+	                                                +"<select name='milk'>"
+                                                    	+ "<option name='milk' value='none' checked>없음</option>"
+	                                                    + "<option name='milk' value='default' checked>일반</option>"
+	                                                    + "<option name='milk' value='soy'>두유</option>"
+	                                                    + "<option name='milk' value='lowfat'>저지방</option>"
+	                                                + "</select></li>"
+                                                + "<li><label>얼음</label>"
+                                                    +"<select name='ice'>"
+                                                        + "<option name='ice' value='1' checked>적게</option>"
+                                                        + "<option name='ice' value='2'>보통</option>"
+                                                        + "<option name='ice' value='3'>많이</option>"
+                                                    + "</select></li>"
+                                                + "<li><label>시럽</label>"
+                                                    +"<select name='syrup'>"
+                                                        + "<option name='syrup' value='none' checked>없음</option>"
+                                                        + "<option name='syrup' value='classic'>클래식시럽</option>"
+                                                        + "<option name='syrup' value='vanilla'>바닐라시럽</option>"
+                                                        + "<option name='syrup' value='hazelnut'>헤이즐넛시럽</option>"
+                                                    + "</select></li>"
+                                                    + "<li><label>휘핑크림</label>"
+                                                    +"<select name='cream'>"
+                                                        + "<option name='cream' value='none' checked>없음</option>"
+                                                        + "<option name='cream' value='regular'>보통</option>"
+                                                        + "<option name='cream' value='extra'>많이</option>"
+                                                    + "</select></li>"
+                                                // + "<li><label>수량</label><input id='"+index+"' onchange='update_amount(this)' type='number' value='"+item.amount+"' min='0' ></li> <br/>"
+                                            + "</ul>"
+                                          +"</form>"
+                                    + "</div>"
+                                +"</div>"
+                                + "<span class='price' id='price"+index+"' align='right'>"+item.price+"원</span>"
+                            +"</div>"
+                        )
+        });
+
+        sum_html = "<hr><br><br><h2 id='total_price' align='right'>" + sum + "원</h2>";
+        console.log(cart);
+    } else {
+        var items_html = "<h3>장바구니가 비어있어요!</h3>"
+        sum_html = "";
+    }
+
+    cart_items.html(items_html + sum_html);
+}
+
+function remove_item(index){
+    var size = cart[index].size;
+    snackBar(size_dictionary[size]+" 사이즈 "+cart[index].name+"가 삭제됐습니다!");
+
+    cart.splice(index, 1);
+
+    // $("#card"+index).hide();
+    // $("div#" + index).remove();
+    show_items();
+}
+
+// TODO: 수량은 계산할 필요 없는 것 같음...
+
+// function update_amount(card_obj){
+//     console.log(card_obj);
+//     var amount = card_obj.value;
+//     var index = card_obj.id;
+
+//     console.log("input number amount changed: " + amount);
+//     if(amount == 0) {
+//         remove_item(card_obj.id);
+//     } else {
+//         cart[index].amount = amount;
+//         var price_span = document.getElementById("price"+index);
+//         price_span.innerHTML = amount * cart[index].price;
+//     }
+// }
+
+
+function update_shot(card_obj){
+    console.log("shot update!");
+    var shots = card_obj.value;
+    var index = card_obj.id.substr(4);
+    var price = 0;
+
+    if(shots == 5) {
+        snackBar("에스프레소 샷은 최대 5개 입니다!");
+    } 
+
+    price = cart[index].price + (500 * (shots - cart[index].shots));
+    $("#price"+index).html(price+"원");
+    get_total_price();
+    console.log("장바구니 사이즈 : "+cart[index].size);
+}
+
+function get_total_price(){
+    var total_price = 0;
+
+    $("span.price").each(function(){
+        console.log(this);
+        var price_per_unit = Number(this.innerText.replace("원", ""));
+        total_price += price_per_unit;
+    });
+
+    $("#total_price").text(total_price + "원");
+
+}
+
+function snackBar(msg) {
+    $("#snackbar").html(msg);
+
+    var x = document.getElementById("snackbar");
+    x.className = "show";
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
