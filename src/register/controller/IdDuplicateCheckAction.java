622084a92076b83aa76c1a0dc90de58f592c7be9
Juhyejeong <66213022+Juhyejeong@users.noreply.github.com>
2020-06-22 22:16:34 +0900

    #3 Create Command.properties

---
+++ src/register/controller/IdDuplicateCheckAction.java
@@ -0,0 +1,16 @@
+package register.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import common.controller.AbstractController;
+
+public class IdDuplicateCheckAction extends AbstractController {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+
+	}
+
+}
