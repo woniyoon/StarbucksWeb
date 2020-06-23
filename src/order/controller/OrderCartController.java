fbb37849c4aca57ad1da3c08e3ea17438a3966f0
woniyoon <gellifisch@gmail.com>
2020-06-24 00:55:09 +0900

    Implement fetching shopping cart

--- src/order/controller/OrderCartController.java
+++ src/order/controller/OrderCartController.java
@@ -1,9 +1,14 @@
 package order.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.util.*;
+
+import javax.servlet.http.*;
+
+import org.json.*;
 
 import common.controller.AbstractController;
+import order.model.*;
+import product.model.*;
 
 public class OrderCartController extends AbstractController {
 
@@ -12,9 +17,41 @@ public class OrderCartController extends AbstractController {
 
 		System.out.println("OrderCartController가 실행중입니다.");
 		
+//		String userid = request.getSession().getAttribute("userid");
+		String userid = "woniyoon";
+		Map<String, String> paramap = new HashMap<String, String>();
+		paramap.put("userid", userid);
+		
+		InterOrderDAO dao = new OrderDAO();
+		List<ProductVO> shoppingCart = dao.getShoppingCart(paramap);
+		
+//		System.out.println(shoppingCart.size());
+//		JSONArray jArray = new JSONArray();
+//		
+//		for(ProductVO pvo : shoppingCart) {
+//			JSONObject obj = new JSONObject();
+//			obj.put("id", pvo.getProductId());
+//			obj.put("name", pvo.getName());
+//			obj.put("price", pvo.getPrice());
+//			obj.put("img", pvo.getImg());
+//			
+//			if(pvo instanceof DrinkVO) {
+//				obj.put("shot", ((DrinkVO) pvo).getShot());
+//				obj.put("syrup", ((DrinkVO) pvo).getSyrup());
+//				obj.put("whipped_cream", ((DrinkVO) pvo).getWhippedCream());
+//				obj.put("temperature", ((DrinkVO) pvo).getTemperature());
+//				obj.put("base", ((DrinkVO) pvo).getBase());
+//			}
+//
+//			jArray.put(obj);
+//		}
+//		
+//		System.out.println(jArray.toString());
+		request.setAttribute("cart", shoppingCart);
+		
 //		super.setRedirect(false);
 		super.setViewPage("/WEB-INF/order/order_cart.jsp");
-		
+
 	}
 
 }
--- src/order/model/OrderDAO.java
+++ src/order/model/OrderDAO.java
@@ -48,20 +48,101 @@ public class OrderDAO implements InterOrderDAO {
 
 	@Override
 	public List<ProductVO> getShoppingCart(Map<String, String> paramap) throws SQLException {
-		return null;
+//		ShoppingCartVO shoppingCart = null;
+		List<ProductVO> items = new ArrayList<ProductVO>();
+		
+		try {
+//			shoppingCart = new ShoppingCartVO();
+
+			conn = ds.getConnection();
+			
+			String sql = " select a.product_id, b.parent_table " + 
+					" from shoppingcart a, nutrition b " + 
+					" where a.userid = ? and a.product_id = b.product_id ";
+			
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, paramap.get("userid"));
+			rs = pstmt.executeQuery();
+		
+			
+			while(rs.next()) {
+				String productID = rs.getString(1);
+				String parentTable = rs.getString(2);
+				
+				System.out.println("productID  : "+ productID);
+				System.out.println("parentTable  : "+ parentTable);
+				System.out.println("getShoppingCart() : " + items.size());
+				
+				if("food".equals(parentTable)) {
+					System.out.println("food!");
+					FoodVO fvo = getFoodInfo(productID);
+					items.add(fvo);
+				} else {
+					System.out.println("drink!");
+
+					DrinkVO dvo = getDrinkInfo(productID);
+					items.add(dvo);
+				}
+			}
+			
+		} finally {
+			close();
+		}
+		
+		return items;
 	}
 
 	@Override
 	public FoodVO getFoodInfo(String productID) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		FoodVO fvo = null;
+
+		conn = ds.getConnection();
+
+		String sql = " select id, name, price, img from food where id = ? ";
+		pstmt = conn.prepareStatement(sql);
+		pstmt.setString(1, productID);
+		rs = pstmt.executeQuery();
+		
+		if(rs.next()) {
+			fvo = new FoodVO();
+			fvo.setProductId(rs.getString("id"));
+			fvo.setName(rs.getString("name"));
+			fvo.setPrice(rs.getInt("price"));
+			fvo.setImg(rs.getString("img"));
+		}
+			
+		return fvo;
 	}
+
+
 
 	@Override
 	public DrinkVO getDrinkInfo(String productID) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
-	}	
-	
+		
+		DrinkVO dvo = null;
+		
+		conn = ds.getConnection();
+		String sql = " select id, name, price, img, shot, syrup, whipped_cream, temperature, base "
+					+ "from drink where id = ? ";
+		
+		pstmt = conn.prepareStatement(sql);
+		pstmt.setString(1, productID);
+		rs = pstmt.executeQuery();
+		
+		if(rs.next()) {
+			dvo = new DrinkVO();
+			dvo.setProductId(rs.getString("id"));
+			dvo.setName(rs.getString("name"));
+			dvo.setPrice(rs.getInt("price"));
+			dvo.setImg(rs.getString("img"));
+			dvo.setShot(rs.getInt("shot"));
+			dvo.setSyrup(rs.getString("syrup"));
+			dvo.setWhippedCream(rs.getString("whipped_cream"));
+			dvo.setTemperature(rs.getString("temperature"));
+			dvo.setBase(rs.getString("base"));
+		}
+		
+		return dvo;
+	}
 	
 }
