b61039867cc275cb2c4d1346f0d8348470e0593b
woniyoon <gellifisch@gmail.com>
2020-06-24 19:37:49 +0900

    Replace ProductVO class

--- src/product/model/ProductVO.java
+++ src/product/model/ProductVO.java
@@ -1,114 +1,113 @@
 package product.model;
 
 public class ProductVO {
-	
-	private String productId;
-	private String categoryCode;
-	private String productSeq;
-	private String name;
-	private String nameEng;
-	private String description;
-	private int price;
-	private String img;
-	private NutritionVO nutrition;
-	private String custom = "";
-	
-	
-	public ProductVO() {
-		
-	}
+   
+   private String parentTable;
+   private String productId;
+   private String categoryID;
+   /* private String productSeq; */
+   private String name;
+   private String nameEng;
+   private String description;
+   private int price;
+   private String img;
+   private NutritionVO nutrition;
+   private String custom = "";
+   
+   public ProductVO() {
+      
+   }
 
-	public ProductVO(String productId, String categoryCode, String productSeq, String name, String nameEng,
-			String description, int price, String img) {
-		super();
-		this.productId = productId;
-		this.categoryCode = categoryCode;
-		this.productSeq = productSeq;
-		this.name = name;
-		this.nameEng = nameEng;
-		this.description = description;
-		this.price = price;
-		this.img = img;
-	}
+   public ProductVO(String parentTable, String productId, String categoryID, String name, String nameEng,
+         String description, int price, String img) {
+      super();
+      this.parentTable = parentTable;
+      this.productId = productId;
+      this.categoryID = categoryID;
+      this.name = name;
+      this.description = description;
+      this.price = price;
+      this.img = img;
+   }
 
-	public String getProductId() {
-		return productId;
-	}
+   public String getParentTable() {
+      return parentTable;
+   }
 
-	public void setProductId(String productId) {
-		this.productId = productId;
-	}
+   public void setParentTable(String parentTable) {
+      this.parentTable = parentTable;
+   }
+   
+   public String getProductId() {
+      return productId;
+   }
 
-	public String getCategoryCode() {
-		return categoryCode;
-	}
+   public void setProductId(String productId) {
+      this.productId = productId;
+   }
 
-	public void setCategoryCode(String categoryCode) {
-		this.categoryCode = categoryCode;
-	}
+   public String getcategoryID() {
+      return categoryID;
+   }
 
-	public String getProductSeq() {
-		return productSeq;
-	}
+   public void setcategoryID(String categoryID) {
+      this.categoryID = categoryID;
+   }
 
-	public void setProductSeq(String productSeq) {
-		this.productSeq = productSeq;
-	}
+   public String getName() {
+      return name;
+   }
 
-	public String getName() {
-		return name;
-	}
+   public void setName(String name) {
+      this.name = name;
+   }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+   public String getNameEng() {
+      return nameEng;
+   }
 
-	public String getNameEng() {
-		return nameEng;
-	}
+   public void setNameEng(String nameEng) {
+      this.nameEng = nameEng;
+   }
 
-	public void setNameEng(String nameEng) {
-		this.nameEng = nameEng;
-	}
+   public String getDescription() {
+      return description;
+   }
 
-	public String getDescription() {
-		return description;
-	}
+   public void setDescription(String description) {
+      this.description = description;
+   }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+   public int getPrice() {
+      return price;
+   }
 
-	public int getPrice() {
-		return price;
-	}
+   public void setPrice(int price) {
+      this.price = price;
+   }
 
-	public void setPrice(int price) {
-		this.price = price;
-	}
+   public String getImg() {
+      return img;
+   }
 
-	public String getImg() {
-		return img;
-	}
-
-	public void setImg(String img) {
-		this.img = img;
-	}
-	
-	public NutritionVO getNutrition() {
-		return nutrition;
-	}
-	
-	public void setNutrition(NutritionVO nutrition) {
-		this.nutrition = nutrition;
-	}
-
-	public String getCustom() {
-		return custom;
-	}
-	
-	public void setCustom(String custom) {
-		this.custom = custom;
-	}
-	
+   public void setImg(String img) {
+      this.img = img;
+   }
+   
+   public NutritionVO getNutrition() {
+      return nutrition;
+   }
+   
+   public void setNutrition(NutritionVO nutrition) {
+      this.nutrition = nutrition;
+   }
+   
+   public String getCustom() {
+      return custom;
+   }
+   
+   public void setCustom(String custom) {
+      this.custom = custom;
+   }
+   
 }
