ef67e8155a15929632c86f675f4b95c7eaf0d6a8
bobaebobae <65759634+bobaebobae@users.noreply.github.com>
2020-06-24 20:51:27 +0900

    Pass data from DB

--- src/product/controller/ProductMenuController.java
+++ src/product/controller/ProductMenuController.java
@@ -1,16 +1,28 @@
 package product.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import common.controller.AbstractController;
-
+import product.model.InterProductDAO;
+import product.model.ProductDAO;
+import product.model.ProductVO;
 public class ProductMenuController extends AbstractController {
 
 	@Override
-	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {  //dao 갖고오자~
+		
+		InterProductDAO pdao = new ProductDAO();
+		
+		List<ProductVO> productList = pdao.getProductList();
+		
+		request.setAttribute("productList", productList);
+		
+//		request.setAttribute("nutritionList", nutritionList);
 		
 		super.setViewPage("/WEB-INF/product_view/product_menu.jsp");
-	}
+	} 
 
 }
