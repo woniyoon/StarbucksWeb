0944977d13b84992151db4586288d88af8b6bddc
woniyoon <gellifisch@gmail.com>
2020-06-25 23:39:19 +0900

    Create ShoppingCartVO

--- src/order/model/ShoppingCartVO.java
+++ src/order/model/ShoppingCartVO.java
@@ -1,1 +1,36 @@
+package order.model;
+
+import java.util.List;
+
+import product.model.ProductVO;
+
+public class ShoppingCartVO {
+
+	private String userid;
+	private String itemSeq;
+	private ProductVO product;
+	
+	public ShoppingCartVO() {
+		
+	}
+	
 	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public String getItemSeq() {
+		return itemSeq;
+	}
+	public void setItemSeq(String itemSeq) {
+		this.itemSeq = itemSeq;
+	}
+	public ProductVO getProduct() {
+		return product;
+	}
+	public void setCart(ProductVO product) {
+		this.product = product;
+	}
+	
+}
