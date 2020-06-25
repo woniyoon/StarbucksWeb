1e333dcf2c2a58bad2efa271cc33003f54de1791
Juhyejeong <66213022+Juhyejeong@users.noreply.github.com>
2020-06-26 00:39:33 +0900

    Update InterMemberDAO.java

--- src/member/model/InterMemberDAO.java
+++ src/member/model/InterMemberDAO.java
@@ -1,6 +1,7 @@
 package member.model;
 
 import java.sql.SQLException;
+import java.util.HashMap;
 
 public interface InterMemberDAO {
 
@@ -9,6 +10,10 @@ public interface InterMemberDAO {
 
 	// ID중복 검사 (userid가 중복이 없어서 사용가능하다라면 true, userid가 이미 존재하여 사용 불가능하면 false 를 리턴) 
 	boolean idDuplicateCheck(String userid) throws SQLException;
+
+	// 아이디와 암호를 입력받아서 그 회원에 대한 정보를 리턴(로그인처리)
+	MemberVO selectOneMember(HashMap<String, String> paraMap) throws SQLException;
+
 
 	
 }
