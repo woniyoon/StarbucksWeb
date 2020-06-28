b3a3946b0567203243b2135f165bd9ee4842a145
woniyoon <gellifisch@gmail.com>
2020-06-28 22:29:29 +0900

    Re-define getAllMembers method

--- src/admin/model/InterAdminDAO.java
+++ src/admin/model/InterAdminDAO.java
@@ -8,5 +8,8 @@ import member.model.MemberVO;
 public interface InterAdminDAO {
 
 	// 전체 회원 목록 가져오기
-	List<MemberVO> getAllMembers(String filterCondition) throws SQLException;
+	List<MemberVO> getAllMembers(HashMap<String, String> paraMap) throws SQLException;
+	
+	// 전체 회원 수 가져오기
+	int getNumOfMems() throws SQLException;
 }
