31460ba807e0067b702928740f9fb7bae9afce35
bobaebobae <65759634+bobaebobae@users.noreply.github.com>
2020-06-29 21:27:00 +0900

    Pass one product to view layer

--- src/product/controller/ProductDetailController.java
+++ src/product/controller/ProductDetailController.java
@@ -14,23 +14,37 @@ public class ProductDetailController extends AbstractController {
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		
 		String productId = request.getParameter("productId"); // 제품번호
+		String type = request.getParameter("type"); // 제품번호
+
+		
+		System.out.println("type : "+type);
 		
 		InterProductDAO pdao = new ProductDAO();
 		
 		List<ProductVO> productList = pdao.getProductList();		
+		ProductVO pvo;
 		
 		/*for(ProductVO pvo : productList) {
 			NutritionVO nvo = pdao.getNutritionInfo(pvo.getProductId());
 			pvo.setNutrition(nvo);
 		}
 		
+		
 		request.setAttribute("productList", productList);*/
 		
+		
 		// 제품번호를 가지고서 해당 제품의 정보를 조회해오기
-		ProductVO pvo = pdao.selectOneProductByProductId(productId);
+		
+		if("DRINK".equalsIgnoreCase(type)) {
+			pvo = pdao.selectOneDrinkByID(type, productId);
+		} else {
+			pvo = pdao.selectOneFoodByID(type, productId);
+		}
+		
+		 
 		
 		// 제품번호를 가지고서 해당 제품의 추가된 이미지 정보를 조회해오기
-		List<String> imgList = pdao.getImagesByProductId(productId);
+		//List<String> imgList = pdao.getImagesByProductId(productId);
 		
 		if(pvo == null) {
 			// GET 방식이므로 사용자가 웹브라우저 주소창에서 장난쳐서 존재하지 않는 제품번호를 입력한 경우
@@ -48,7 +62,7 @@ public class ProductDetailController extends AbstractController {
 		else {
 			// 제품이 있는 경우
 			request.setAttribute("pvo", pvo);
-			request.setAttribute("imgList", imgList);
+//			request.setAttribute("imgList", imgList);
 			//request.setAttribute("goBackURL", MyUtil.getCurrentURL(request));
 		
 			//	super.setRedirect(false);
