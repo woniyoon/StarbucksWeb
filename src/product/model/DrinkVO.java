d1ff221ba1e93307996a07bab0ec56ab5b3ce5b0
bobaebobae <65759634+bobaebobae@users.noreply.github.com>
2020-06-29 21:26:13 +0900

    Add properties in DrinkVO

--- src/product/model/DrinkVO.java
+++ src/product/model/DrinkVO.java
@@ -21,4 +21,54 @@ public class DrinkVO extends ProductVO {
 		this.caffein = caffein;
 		this.base = base;
 	}
+
+	public int getShot() {
+		return shot;
+	}
+
+	public void setShot(int shot) {
+		this.shot = shot;
+	}
+
+	public String getSyrup() {
+		return syrup;
+	}
+
+	public void setSyrup(String syrup) {
+		this.syrup = syrup;
+	}
+
+	public String getWhippedCream() {
+		return whippedCream;
+	}
+
+	public void setWhippedCream(String whippedCream) {
+		this.whippedCream = whippedCream;
+	}
+
+	public String getTemperature() {
+		return temperature;
+	}
+
+	public void setTemperature(String temperature) {
+		this.temperature = temperature;
+	}
+
+	public int getCaffein() {
+		return caffein;
+	}
+
+	public void setCaffein(int caffein) {
+		this.caffein = caffein;
+	}
+
+	public String getBase() {
+		return base;
+	}
+
+	public void setBase(String base) {
+		this.base = base;
+	}
+	
+	
 }
