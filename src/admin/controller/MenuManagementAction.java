70d9149a60d4597013cd293441e7fce5e31a0788
woniyoon <gellifisch@gmail.com>
2020-07-05 01:26:17 +0900

    Fix a wrong url path

--- src/admin/controller/MenuManagementAction.java
+++ src/admin/controller/MenuManagementAction.java
@@ -1,7 +1,6 @@
 package admin.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
 
 import common.controller.AbstractController;
 
@@ -10,7 +9,7 @@ public class MenuManagementAction extends AbstractController {
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		
-		super.setViewPage("/WEB-INF/admin/menu_management.sb");
+		super.setViewPage("/WEB-INF/admin/menu_management.jsp");
 	}
 
 }
