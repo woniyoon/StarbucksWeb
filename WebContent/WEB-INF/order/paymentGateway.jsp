4b2b19429a774b537d28e168f7de44cebfa66bcd
woniyoon <gellifisch@gmail.com>
2020-07-04 20:22:55 +0900

    Organize lines of code

--- WebContent/WEB-INF/order/paymentGateway.jsp
+++ WebContent/WEB-INF/order/paymentGateway.jsp
@@ -12,51 +12,41 @@
 <script type="text/javascript">
 
 $(document).ready(function() {
-	
-	console.log(sessionStorage.getItem("price_to_pay"));
+
+	//필요없는거 같음 ;;;;;
+/* 	console.log(sessionStorage.getItem("price_to_pay"));
 	console.log(sessionStorage.getItem("store_id"));
 	console.log(sessionStorage.getItem("deducted_point"));
 	console.log(sessionStorage.getItem("product_id"));
 	console.log(sessionStorage.getItem("item_seq"));
-	console.log(sessionStorage.getItem("price_per_item"));
+	console.log(sessionStorage.getItem("price_per_item")); */
 	
 	var amount = sessionStorage.getItem("price_to_pay");
 	/* var slip_no = ${slipNo}; */
 	/* console.log(slip_no); */
 	
 	amount /= 100;
-	//	여기 링크를 꼭 참고하세용 http://www.iamport.kr/getstarted
-   var IMP = window.IMP;     // 생략가능
-   IMP.init("imp38336214");  // 중요!!  아임포트에 가입시 부여받은 "가맹점 식별코드". 
+
+	var IMP = window.IMP; 
+   
+	IMP.init("imp38336214");
 	
-   // 결제요청하기
-   IMP.request_pay({
-       pg : "html5_inicis", // 결제방식 PG사 구분
-       pay_method : "card",	// 결제 수단
-       merchant_uid : "merchant_" + new Date().getTime(), // 가맹점에서 생성/관리하는 고유 주문번호
-       name : "(주) 스타벅스",	 // 전표아이디
-       amount : amount,	  // '${coinmoney}'  결제 금액 number 타입. 필수항목. 
-       buyer_email : "${sessionScope.loginuser.email}",  // 구매자 email
-       buyer_name : "${sessionScope.loginuser.name}",	  // 구매자 이름 
-       buyer_tel : "${sessionScope.loginuser.phone}",    // 구매자 전화번호 (필수항목)
-       m_redirect_url : ""  // 휴대폰 사용시 결제 완료 후 action : 컨트롤러로 보내서 자체 db에 입력시킬것!
-   }, function(rsp) {
+	// 결제요청하기
+	IMP.request_pay({
+       pg : "html5_inicis",
+       pay_method : "card",
+       merchant_uid : "merchant_" + new Date().getTime(),
+       name : "(주) 스타벅스",	
+       amount : amount,
+       buyer_email : "${sessionScope.loginuser.email}",
+       buyer_name : "${sessionScope.loginuser.name}",	 
+       buyer_tel : "${sessionScope.loginuser.phone}",
+       m_redirect_url : ""
+   	}, function(rsp) {
 
-		if ( rsp.success ) { // PC 데스크탑용
-		/* === 팝업창에서 부모창 함수 호출 방법 3가지 ===
-		    1-1. 일반적인 방법
-			opener.location.href = "javascript:부모창스크립트 함수명();";
-			opener.location.href = "http://www.aaa.com";
+		if ( rsp.success ) { 
 			
-			1-2. 일반적인 방법
-			window.opener.부모창스크립트 함수명();
-
-			2. jQuery를 이용한 방법
-			$(opener.location).attr("href", "javascript:부모창스크립트 함수명();");
-		*/
-		//	opener.location.href = "javascript:goCoinUpdate('${idx}','${coinmoney}');";
 			window.opener.update_database();
-		//  $(opener.location).attr("href", "javascript:goCoinUpdate('${idx}','${coinmoney}');");
 			
 		    self.close();
 			
@@ -65,9 +55,8 @@ $(document).ready(function() {
             alert("결제에 실패하였습니다. \n 다시 시도해주세요!");
        }
 
-   }); // end of IMP.request_pay()----------------------------
-
-}); // end of $(document).ready()-----------------------------
+   }); 
+}); 
 
 </script>
 </head>	
--- src/order/controller/UpdatePurchaseAction.java
+++ src/order/controller/UpdatePurchaseAction.java
@@ -4,6 +4,8 @@ import java.util.*;
 
 import javax.servlet.http.*;
 
+import org.json.JSONObject;
+
 import common.controller.AbstractController;
 import order.model.*;
 import member.model.*;
@@ -14,28 +16,31 @@ public class UpdatePurchaseAction extends AbstractController {
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		
 		String userid = ((MemberVO) request.getSession().getAttribute("loginuser")).getUserid();
+		Integer point = ((MemberVO) request.getSession().getAttribute("loginuser")).getPoint();
 		String storeId = request.getParameter("storeId");
-		String[] pricePerItems = request.getParameterValues("pricePerItems");
-		String[] productIds = request.getParameterValues("productIds");
-		String[] itemSeqArr = request.getParameterValues("itemSeqArr");
+		System.out.println(request.getParameter("pricePerItems"));
+		System.out.println(request.getParameter("storeId"));
+
+		String[] pricePerItems = request.getParameter("pricePerItems").split(",");
+		
+		String[] productIds = request.getParameter("productIds").split(",");
+		String[] itemSeqArr = request.getParameter("itemSeqArr").split(",");	// shoppingcart_seq;
 		String pricePaid = request.getParameter("pricePaid");
 		String deductedPoint = request.getParameter("deductedPoint");
 		String slipNo = request.getParameter("slipNo");
 		
 		InterOrderDAO dao = new OrderDAO();
 
-		// 1. 전표 업데이트 (전표번호/아이디/매장아이디/판매일자/전표시퀀스)		
 		// 전표 객체 생성
 		SlipVO slip = new SlipVO();
 		slip.setSlipNum(slipNo);
 		slip.setStoreId(storeId);
 		slip.setUserid(userid);
 		
-		dao.insertSlip(slip);
 		
-		// 2. 구매상세 업데이트 (제품아이디/전표번호/아이디/매장아이디/주문수량/금액/분류/구매상세시퀀스)
 		// 구매상세 객체 생성
 		List<PurchaseDetailVO> pdvList = new ArrayList<>();
+		
 		for(int i=0; i<productIds.length; i++) {
 			
 			PurchaseDetailVO pdvo = new PurchaseDetailVO();
@@ -44,7 +49,7 @@ public class UpdatePurchaseAction extends AbstractController {
 			pdvo.setSlipNum(slipNo);
 			pdvo.setUserid(userid);
 			pdvo.setStoreId(storeId);
-			pdvo.setQty(1);
+			pdvo.setCnt(1);
 			pdvo.setPrice(Integer.parseInt(pricePerItems[i]));
 			
 			if("F".equalsIgnoreCase(pdvo.getProductId().substring(0,1))) {
@@ -52,13 +57,33 @@ public class UpdatePurchaseAction extends AbstractController {
 			} else {
 				pdvo.setSection(0);
 			}
+			
+			pdvList.add(pdvo);
+			System.out.println("productIds 반복문 횟수 : " +i);
  		}
 		
+		Map<String, Object> paramap = new HashMap<>();
+		paramap.put("slip", slip);
+		paramap.put("pdvList", pdvList);
+		paramap.put("userid", userid);
+		paramap.put("itemSeqArr", itemSeqArr);
+		paramap.put("pricePaid", pricePaid);
+		paramap.put("deductedPoint", deductedPoint);
+		paramap.put("point", point);
+
+		System.out.println(" 왜이래 ㅡㅡ ");
 		
-		// 3. 장바구니 삭제 (아이디/아이템시퀀스)
+		boolean isUpdated = dao.updateConfirmedPurchase(paramap);
+
+		System.out.println("~~~~ isUpdated : " + isUpdated);
 		
-		// 4. 포인트 업데이트 & 적립금
+		JSONObject jsonObj = new JSONObject();
+		jsonObj.put("isUpdated", isUpdated);
+		String json = jsonObj.toString();
 		
+		System.out.println(json);
+		
+		request.setAttribute("json", json);
 		
 		super.setViewPage("/WEB-INF/order/result.jsp");
 	}
