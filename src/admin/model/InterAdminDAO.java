5761a15b5143fd2224f6105a8c0f190e5951c0c2
woniyoon <gellifisch@gmail.com>
2020-07-04 21:47:10 +0900

    Create getDrinkList method

--- src/admin/model/InterAdminDAO.java
+++ src/admin/model/InterAdminDAO.java
@@ -4,6 +4,7 @@ import java.sql.SQLException;
 import java.util.*;
 
 import member.model.MemberVO;
+import product.model.ProductVO;
 
 public interface InterAdminDAO {
 
@@ -12,4 +13,9 @@ public interface InterAdminDAO {
 	
 	// 전체 회원 수 가져오기
 	int getNumOfMems() throws SQLException;
+
+	// 전체 음료 목록 가져오기
+	List<ProductVO> getDrinkList(HashMap<String, String> paramap) throws SQLException;
+	
+	//
 }
