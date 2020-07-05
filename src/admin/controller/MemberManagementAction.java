93863fb1a88ae93bed48f211e8771750c34ac02c
woniyoon <gellifisch@gmail.com>
2020-07-05 17:20:35 +0900

    Add preventing from unauthorized user

--- src/admin/controller/MemberManagementAction.java
+++ src/admin/controller/MemberManagementAction.java
@@ -13,6 +13,36 @@ public class MemberManagementAction extends AbstractController {
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
+		
+		if(!checkLogin(request)) {
+			
+			String message = "먼저 로그인 해야 가능합니다.";
+			String loc = "javascript:history.back()";
+			
+			request.setAttribute("message", message);
+			request.setAttribute("loc", loc);
+			
+			super.setRedirect(false);
+			super.setViewPage("/WEB-INF/msg.jsp");
+			
+			return;
+			
+		} else {
+			if(!"admin".equalsIgnoreCase(getLoginUser(request).getUserid())) {
+				String message = "잘못된 경로입니다.";
+				String loc = "javascript:history.back()";
+				
+				request.setAttribute("message", message);
+				request.setAttribute("loc", loc);
+				
+				super.setRedirect(false);
+				super.setViewPage("/WEB-INF/msg.jsp");
+				
+				return;
+			}
+			
+		}	
+		
 			
 		String currentShowPageNo = request.getParameter("currentShowPageNo");			
 		String filterCondition = request.getParameter("filterCondition");			
--- src/admin/controller/MenuManagementAction.java
+++ src/admin/controller/MenuManagementAction.java
@@ -8,8 +8,38 @@ public class MenuManagementAction extends AbstractController {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		
-		super.setViewPage("/WEB-INF/admin/menu_management.jsp");
+
+		if(!checkLogin(request)) {
+			
+			String message = "먼저 로그인 해야 가능합니다.";
+			String loc = "javascript:history.back()";
+			
+			request.setAttribute("message", message);
+			request.setAttribute("loc", loc);
+			
+			super.setRedirect(false);
+			super.setViewPage("/WEB-INF/msg.jsp");
+			
+			return;
+			
+		} else {
+			if(!"admin".equalsIgnoreCase(getLoginUser(request).getUserid())) {
+				String message = "잘못된 경로입니다.";
+				String loc = "javascript:history.back()";
+				
+				request.setAttribute("message", message);
+				request.setAttribute("loc", loc);
+				
+				super.setRedirect(false);
+				super.setViewPage("/WEB-INF/msg.jsp");
+				
+				return;
+			} else {
+				super.setViewPage("/WEB-INF/admin/menu_management.jsp");
+			}
+			
+		}	
+
 	}
 
 }
